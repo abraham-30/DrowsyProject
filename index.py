ceca66426fa80553e387a457e97e5d64f171f804
MichaelScu <michaelscuderiatanudjaja@gmail.com>
2023-09-23 22:19:16 +0700

    masi nyoba
    baru kameranya doang yang masuk tapi belom bisa detect sama pilih suara

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 import os
 import pygame
 from tkinter import Tk, Label, Button, filedialog
@@ -320,26 +320,30 @@ camera=cv.VideoCapture(0)
 
 def generate_frames():
     while True:
-        success, frame=camera.read()
+        success, frame = camera.read()
         if not success:
             break
         else:
-            ret,buffer=cv.imencode('.jpg', frame)
-            frame=buffer.tobytes()
+            ret, buffer = cv.imencode('.jpg', frame)
+            frame = buffer.tobytes()
         
-        yield(b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
+        yield (b'--frame\r\n' 
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 @app.route("/")
-def home():
-    return render_template('index.html')
+def index():
+    return render_template("index.html")
 
 @app.route('/video')
 def video():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+
 if __name__ == "__main__":
-    root = Tk()
-    app = DrowsinessDetectionApp(root)
-    root.mainloop()
-    app.run()
+    app.run(debug=True)
+    
+# if __name__ == "__main__":
+#     root = Tk()
+#     app = DrowsinessDetectionApp(root)
+#     root.mainloop()
+#     app.run()
+    # index.run(debug=True)
